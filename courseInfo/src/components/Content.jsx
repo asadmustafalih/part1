59c0c9fbfc2b9eb050980f3f18042e410e44901b
asadmustafalih <158236672+asadmustafalih@users.noreply.github.com>
2024-08-29 17:05:27 +0200

    completed part1

---
+++ courseInfo/src/components/Content.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import Part from "./Parts";
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  );
+};
+
+export default Content;
